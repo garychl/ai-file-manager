55d5639f9ae9ad235b8a0ca706e443dcce131645
garychl <garychl320@gmail.com>
2020-01-22 14:00:07 +0800

    refactor

---
+++ utilities/utils.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+import yaml
+
+
+def read_yaml_input(yaml_input):
+    """Reading configuration file."""
+    if isinstance(yaml_input, str):
+        with open(yaml_input, 'r', newline='') as f:
+            try:
+                yaml_config = yaml.safe_load(f)
+            except yaml.YAMLError as ymlexcp:
+                print(ymlexcp)
+    elif isinstance(yaml_input, dict):
+        yaml_config = yaml_input
+    else:
+        raise Exception("Only accept dict or path of the .yaml file.")
+    return yaml_config
+
+
+def cd_prj_dir(show=False):
+    sub_folder = ['experiments', 'data']
+    cur_folder = os.path.basename(os.getcwd())
+
+    if cur_folder in sub_folder:
+        os.chdir("../")
+        if show: 
+            prj_dir = os.getcwd() 
+        else:
+            prj_dir = 'project dir.'
+        print('Changed dir to {}'.format(prj_dir))
+
+    assert 'README.md' in os.listdir(), "Cannot find REAME.md. Make sure current dir is the project dir."  
