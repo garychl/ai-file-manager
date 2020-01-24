40c804722af607d85bb4da0ceb252bc33f3d1960
garychl <garychl320@gmail.com>
2020-01-24 14:02:45 +0800

    add parse abstract function

--- app.py
+++ app.py
@@ -1,24 +1,61 @@
 """
 Given a PDF file, generate a set of topics for it and add to the metadata.
 """
-from pdfrw import PdfReader, PdfWriter, IndirectPdfDict 
+import warnings
+from io import StringIO
 from pprint import pprint
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from pdfrw import PdfReader, PdfWriter, IndirectPdfDict 
+
 
 class PdfFileManager():
     def __init__(self, path, model):
-        self.reader = PdfReader(path)
         self.path = path
-        self.model = model 
+        output_string = StringIO()
+        with open(path, 'rb') as in_file:
+            parser = PDFParser(in_file)
+            doc = PDFDocument(parser)
+            rsrcmgr = PDFResourceManager()
+            device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            for page in PDFPage.create_pages(doc):
+                interpreter.process_page(page)
+            self.raw_data = output_string.getvalue()
+        output_string.close()
+
 
     def set_meta_data(self, key, value):
-        setattr(self.reader.Info, key, value)   
-        PdfWriter(self.path, trailer=self.reader).write()
-
+        # using code from other module - see if it can improved later
+        reader = PdfReader(self.path)
+        assert "/"+key not in reader.Info, \
+               "{} already existed in metadata".format(key)
+        setattr(reader.Info, key, value)   
+        PdfWriter(self.path, trailer=reader).write()
+        
+        
     def get_meta_data(self):
-        return self.reader.Info
+        with open(self.path, 'rb') as in_file:
+            parser = PDFParser(in_file)
+            doc = PDFDocument(parser)
+            meta_data = doc.info
+            if len(meta_data) > 1:
+                warnings.warn('len of metadata >1', UserWarning)
+        return meta_data
 
-    def parse_doc(self, content='abstract'):
-        pass
+    def parse_abstract(self):
+        abs_index = self.raw_data.find('Abstract')
+        intro_index = self.raw_data.find('Introduction')
+        if (abs_index == -1 or intro_index == -1):
+            warnings.warn('Could not locate abstract', UserWarning)
+            return None
+        else:
+            return self.raw_data[abs_index:intro_index]
 
     
 if __name__ == '__main__':
